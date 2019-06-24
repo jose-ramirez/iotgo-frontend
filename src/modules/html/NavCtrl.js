858d29e343e1eafc383e9fef8ae0ea604e4ea0a7
Jose Ramirez <a30673083@gmail.com>
2019-06-23 23:35:40 -0300

    Controller refactoring started

--- src/modules/html/NavCtrl.js
+++ src/modules/html/NavCtrl.js
@@ -1,9 +1,25 @@
 import iotgo from '../../app'
 
-iotgo
-  .controller('NavCtrl', [ '$scope', 'User',
-    function ($scope, User) {
-      $scope.isLoggedIn = User.isLoggedIn;
-      $scope.logout = User.logout;
-    }
-  ]);
+// iotgo
+//   .controller('NavCtrl', [ '$scope', 'User',
+//     function ($scope, User) {
+//       $scope.isLoggedIn = User.isLoggedIn;
+//       $scope.logout = User.logout;
+//     }
+//   ]);
+
+class NavController{
+  constructor(User){
+    this.User = User
+  }
+  
+  isLoggedIn(){
+    return this.User.isLoggedIn()
+  }
+
+  logout(){
+    this.User.logout()
+  }
+}
+
+iotgo.controller('NavCtrl', NavController)
